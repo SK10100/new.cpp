2b0939814cc21a22d08e0dc7cf2b466d2c0cf1c2
Sonali Kumari <sonali522003@gmail.com>
2024-03-18 09:22:24 +0530

    Test-increment-decrement_ptoblem

---
+++ TestIncDec.cpp
@@ -0,0 +1,22 @@
+/*Test on increment(++) and decrement (--) operator */
+#include<iostream>
+using namespace std;
+int main(){
+  int mark=76;
+  cout<<mark<<endl;
+  mark++;
+  cout<<mark<<endl;
+  ++mark;
+  cout<<mark<<endl;
+  mark--;
+  cout<<mark<<endl;
+  --mark;
+  cout<<mark<<endl;
+  mark=mark+1;
+  cout<<mark<<endl;
+  mark=mark-1;
+  cout<<mark<<endl;
+
+  return 0;
+
+}
