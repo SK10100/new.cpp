bac697cd19085e32d3ad73b06f321f57d6f18bb7
Sonali Kumari <sonali522003@gmail.com>
2024-03-19 19:01:38 +0530

    if-then-else

---
+++ if-then-else.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int mark;
+  cout<<"Enter your marks : ";
+  cin>>mark;
+  if(mark>=50){
+    cout<<"Congratulation!"<<endl;
+    cout<<"Keep it up!"<<endl;
+  }else{
+    cout<<"Try Harder!"<<endl;
+  
+  }
+
+  return 0;
+}
